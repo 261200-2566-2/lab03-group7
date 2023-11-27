26a881145544caab178e4060745d33cca75c6c61
PAWEENNUTpuyfai <paweennut.pp@outlook.com>
2023-11-27 15:14:03 +0700

    commit current code/design

--- src/Character.java
+++ src/Character.java
@@ -2,52 +2,48 @@ public class Character {
     private static int baseSwordDmg;
     private static int baseShieldDef;
     private static double baseSPD;
-
-    String name;
-    private int level;
+    private String name;
+    private int level = 1;
     private double maxHP;
     // maxHP = 100+(10*level);
     private double currentHP ;
     private double maxMana;
     private double currentMana;
-    private double maxSPD;
-    private double currentSPD;
+    private double SPD;
     private int isSwordEquip = 0;
     private int isShieldEquip = 0;
     private int swordLevel =1;
     private int shieldLevel =1;
-    double dmg;
-    double def;
-    boolean isUnconscious;
+    private double dmg;
+    private double def;
+    private boolean isUnconscious;
+    private int currentExp;
+    private int maxExp;
 
-
-    Character(String name, int level){
+    Character(String name){
         this.name = name;
-        this.level = level;
         maxHP = 100+(10*level);
         currentHP = maxHP;
         maxMana = 50+(2*level);
         currentMana = maxMana;
         baseSPD = 10;
-        maxSPD = baseSPD*(0.1+0.03*level);
+        SPD = baseSPD*(0.1+0.03*level);
         baseSwordDmg = 10;
         baseShieldDef = 10;
         isUnconscious = false;
     }
-
     private void updateStatus(){
         maxHP = 100+(10*level);
         maxMana = 50+(2*level);
-        maxSPD = baseSPD*(0.1+0.03*level);
-        currentSPD = maxSPD - (isSwordEquip*baseSPD*(0.1+0.04*swordLevel)) - (isShieldEquip*baseSPD*(0.1+0.08*level));
+        maxExp = 100*level;
+        SPD = (baseSPD*(0.1+0.03*level))- (isSwordEquip*baseSPD*(0.1+0.04*swordLevel)) - (isShieldEquip*baseSPD*(0.1+0.08*level));
         dmg = isSwordEquip*baseSwordDmg*(1+0.1*swordLevel);
         def = isShieldEquip*baseShieldDef*(1+0.05*shieldLevel);
     }
-    public void characterLevelAscending(){
+    private void characterLevelAscending(){
         level++;
         updateStatus();
     }
-
     public void swordEquip(){
         isSwordEquip = 1;
         updateStatus();
@@ -56,7 +52,6 @@ public class Character {
         isShieldEquip = 1;
         updateStatus();
     }
-
     public void swordUnEquip(){
         isSwordEquip = 0;
         updateStatus();
@@ -79,7 +74,6 @@ public class Character {
         }
         else System.out.println("You didn't equip your shield yet");
     }
-
     public double wasAttacked(double damage){
         double damageTaken = damage - isShieldEquip*def ;
         currentHP = currentHP - damageTaken;
@@ -89,12 +83,11 @@ public class Character {
         }
         return damageTaken;
     }
-
-    public double attack(double opponentDef) {
+    public double attack() {
         if(!isUnconscious) {
             if (currentMana != 0) {
                 currentMana--;
-                return dmg - opponentDef;
+                return dmg;
             }
             System.out.println("You run out of mana");
             return 0;
@@ -114,4 +107,25 @@ public class Character {
         currentHP = maxHP;
         currentMana = maxMana;
     }
+    public void changeName(String n){
+        this.name = n;
+    }
+    public void showStatus(){
+        System.out.println("Name : "+this.name);
+        System.out.println("Level : "+this.level + "exp" + currentExp + " / " + maxExp);
+        System.out.println("HP : " + currentHP + " / " + maxHP);
+        System.out.println("mana : " + currentMana + " / " + maxMana);
+        System.out.println("speed : " + this.SPD);
+        if(isSwordEquip == 1) System.out.println("sword level : " + swordLevel);
+        if(isShieldEquip == 1) System.out.println("shield level : " + shieldLevel);
+    }
+    public void updateExp(int exp){
+        this.currentExp += exp;
+        if(currentExp >= maxExp){
+            currentExp -= maxExp;
+            this.characterLevelAscending();
+            System.out.println("Level up !!");
+        }
+    }
+
 }
